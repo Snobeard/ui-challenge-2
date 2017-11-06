d5abbdd5c9f96215608e60c1a5ccff64777a6b80
Snobeard <fatmattmatt@gmail.com>
2017-11-06 13:50:14 -0800

    added checkbox toggle, radio select, and submit prevent refresh

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,27 @@
 'use strict';
 
-let ui = {};
+let myUI = {};
 
-ui.toggleCheckbox() => {
+myUI.toggleCheckbox = () => {
+  $('#checkbox').on('click', '.check', function() {
+    $(this).prev().click()
+  })
+}
 
+myUI.selectRadio = () => {
+  $('#radio').on('click', '.radio', function() {
+    $(this).prev().click()
+  })
+}
 
+myUI.preventRefresh = () => {
+  $('.ui').submit((e) => {
+    return false;
+  })
+}
+
+myUI.initIndexPage = () => {
+  myUI.toggleCheckbox();
+  myUI.selectRadio();
+  myUI.preventRefresh();
 }
